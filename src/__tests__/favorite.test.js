c2c5b8cdbed17222a753802cc6e625cf5384e427
Marlon Couto <marloncouto199@gmail.com>
2022-11-12 19:19:58 -0300

    Finaliza projeto
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/__tests__/favorite.test.js
+++ src/__tests__/favorite.test.js
@@ -1,8 +1,10 @@
-// const { saveFavoriteMagicCard } = require('../magic');
-// const favoriteCards = require('../data/favoriteCards');
+const { saveFavoriteMagicCard } = require('../magic');
+let favoriteCards = require('../data/favoriteCards');
+
+const favoriteCardsClone = [...favoriteCards];
 
 const restoreFavoriteCards = () => {
-  // implemente seu código aqui
+  favoriteCards = favoriteCardsClone;
 };
 
 jest.setTimeout(10000);
@@ -11,20 +13,36 @@ describe('3 - Testes da função saveFavoriteMagicCard', () => {
   afterEach(restoreFavoriteCards);
 
   it('3.1 Testa se um novo card é adicionado a cada execução', async () => {
-    // await saveFavoriteMagicCard('130553');
-    // implemente seus testes aqui
+    await saveFavoriteMagicCard('130553');
+    expect(favoriteCards.length).toBe(5);
 
+    const { name } = favoriteCards[4];
+    expect(name).toBe('Beacon of Immortality');
+
+    await saveFavoriteMagicCard('130553');
+    expect(favoriteCards.length).toBe(6);
   });
 
   it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect(favoriteCards).toHaveLength(4);
+    expect(favoriteCards).toHaveLength(4);
   });
 });
 
 describe('4 - Testes da função saveFavoriteMagicCard', () => {
   it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
   contém todos os quatro nomes iniciais nomes`, async () => {
-    // implemente seus testes aqui
+    const originalNames = [
+      'Ancestor\'s Chosen',
+      'Angel of Mercy',
+      'Aven Cloudchaser',
+      'Ballista Squad'
+    ];
 
+    const names = favoriteCards.map((favoriteCard) => {
+      const { name } = favoriteCard;
+      return name;
+    });
+
+    expect(names).toEqual(originalNames);
   });
 });
--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -1,27 +1,31 @@
-// const { getMagicCard } = require('../magic');
-// require('../../startest/mockSetup');
+const { getMagicCard } = require('../magic');
+require('../../startest/mockSetup');
 
 jest.setTimeout(10000);
 
 describe('1 - Testes da função getMagicCard', () => {
-  it('1.1 Verifique se getMagicCard é uma função.', () => {
-    // implemente seus testes aqui
+  it('1.1 Verifique se getMagicCard é uma função.', async () => {
+    expect(typeof getMagicCard).toBe('function');
   });
 
   it('1.2 Verifique se a função fetch foi chamada.', async () => {
-    // await getMagicCard('130550');
-    // implemente seus testes aqui
+    await getMagicCard('130550');
+    expect(fetch).toBeCalled();
   });
 
   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
-    // await getMagicCard('130550');
-    // implemente seus testes aqui
+    await getMagicCard('130550');
+    const endpoint = 'https://api.magicthegathering.io/v1/cards/130550';
+    
+    expect(fetch).toBeCalledWith(endpoint);
   });
 });
 
 describe('2 - Testando o retorno da função getMagicCard', () => {
   it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
-    // const response = await getMagicCard('130550');
-    // implemente seus testes aqui
+    const response = await getMagicCard('130550');
+    const { name } = response;
+    
+    expect(name).toBe('Ancestor\'s Chosen');
   });
 });
